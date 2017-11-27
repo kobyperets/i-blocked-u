370bd3eb7707c171166a83eb1117f160aa97ab3a
dlevi <doron.levi@salesforce.com>
2017-11-27 15:33:36 +0200

    Say hello

---
+++ IBlockedU/src/main/java/com/salesforce/iblockedu/IBlockedU/controllers/IBlockedUController.java
@@ -0,0 +1,22 @@
+package com.salesforce.iblockedu.IBlockedU.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by doron.levi on 27/11/2017.
+ */
+
+@RestController
+@RequestMapping("/iblockedu/api")
+public class IBlockedUController {
+
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String sayHello(@RequestParam String helloTo) {
+        return "hello " + helloTo;
+    }
+}
+
