cff23cfa47d6d31377341bb24266cf195a85b5af
kperets <kperets@salesforce.com>
2017-11-27 15:10:48 +0200

    initial spring app skeleton

---
+++ IBlockedU/src/main/java/com/salesforce/iblockedu/IBlockedU/IBlockedUApplication.java
@@ -0,0 +1,12 @@
+package com.salesforce.iblockedu.IBlockedU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IBlockedUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IBlockedUApplication.class, args);
+	}
+}
---
+++ IBlockedU/src/test/java/com/salesforce/iblockedu/IBlockedU/IBlockedUApplicationTests.java
@@ -0,0 +1,16 @@
+package com.salesforce.iblockedu.IBlockedU;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IBlockedUApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
