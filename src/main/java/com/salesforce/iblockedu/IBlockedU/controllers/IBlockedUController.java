dd3b9d11e8ed6a8b3cb8d1233e1049f10dcc0e81
dlevi <doron.levi@salesforce.com>
2017-11-29 00:18:14 +0200

    Add DB init

--- src/main/java/com/salesforce/iblockedu/IBlockedU/controllers/IBlockedUController.java
+++ src/main/java/com/salesforce/iblockedu/IBlockedU/controllers/IBlockedUController.java
@@ -1,9 +1,15 @@
 package com.salesforce.iblockedu.IBlockedU.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
 
 /**
  * Created by doron.levi on 27/11/2017.
@@ -13,6 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/iblockedu/api")
 public class IBlockedUController {
 
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Autowired
+    private DataSource dataSource;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String sayHello(@RequestParam String helloTo) {
@@ -32,6 +43,26 @@ public class IBlockedUController {
     @RequestMapping(value = "/signin", method = RequestMethod.GET)
     public String signIn(@RequestParam String email) {
         return email + " is now signed-in";
+    }
+
+    @RequestMapping("/dbinit")
+    String dbinit() {
+        try (Connection connection = dataSource.getConnection()) {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS USERS (ID serial primary key, EMAIL text not null unique, NAME text not null, PHONE_NUMBER text not null, IMAGE_LOCATION text, ACTIVE boolean)");
+            //stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+            /*ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+            ArrayList<String> output = new ArrayList<String>();
+            while (rs.next()) {
+                output.add("Read from DB: " + rs.getTimestamp("tick"));
+            }
+
+            model.put("records", output);*/
+            return "db created";
+        } catch (Exception e) {
+            return "error " + e.getMessage();
+        }
     }
 }
 
