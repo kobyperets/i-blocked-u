9a4922d560b54b938b6215e66093f18b034e9442
dlevi <doron.levi@salesforce.com>
2017-11-29 12:21:42 +0200

    Add DataSource config

---
+++ src/main/java/com/salesforce/iblockedu/IBlockedU/dal/UsersDal.java
@@ -0,0 +1,14 @@
+package com.salesforce.iblockedu.IBlockedU.dal;
+
+import com.salesforce.iblockedu.IBlockedU.model.User;
+
+/**
+ * Created by doron.levi on 29/11/2017.
+ */
+public class UsersDal {
+
+    public void createUser(User user) {
+
+    }
+
+}
---
+++ src/main/java/com/salesforce/iblockedu/IBlockedU/model/User.java
@@ -0,0 +1,8 @@
+package com.salesforce.iblockedu.IBlockedU.model;
+
+/**
+ * Created by doron.levi on 29/11/2017.
+ */
+public class User {
+
+}
